5922a49be67fd44b06e49de6ef30520d7741dd2c
fabriciovd <YavorPetkov@students.softuni.bg>
2024-07-24 11:01:50 +0300

    Added HTML build templates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,10 @@
-import { render } from 'preact'
-import { App } from './app.jsx'
-import './index.css'
+import { render } from "preact";
+import { App } from "./app.jsx";
+import "./index.css";
 
-render(<App />, document.getElementById('app'))
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("app")
+);
